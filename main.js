6a91e4aff4634fa6153bfd47057d12b249dd30da
rastko <vucen.rastko@gmail.com>
2023-04-19 17:13:31 +0200

    Make a program work for every container width and whatever number of squares

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+const GRID_CONTAINER_WIDTH = 580;
+
 // function creates a 16*16 grid (in combination with css) of square divs inside a grid container
 // Use div with break class to break one line after n squares if dimensions are n*n
+// Calculate width and height of a single square based on total width of a grid-container
 function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    const squareSize = GRID_CONTAINER_WIDTH / 20 - 2;
+    console.log(squareSize);
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 20; j++) {
             const div = document.createElement('div');        
             div.className = "square";
+            div.style.height = `${squareSize}px`;
+            div.style.width = `${squareSize}px`;
             gridContainer.appendChild(div);
         }
         const br = document.createElement('div');
